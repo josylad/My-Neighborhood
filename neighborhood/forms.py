9ac88e69c08f462641350ff7d06a2840dec7dd46
Joseph Adediji <josylado@gmail.com>
2019-10-26 19:27:21 +0300

    Update Neighborhood addition form

--- neighborhood/forms.py
+++ neighborhood/forms.py
@@ -11,7 +11,7 @@ class NewBusinessForm(forms.ModelForm):
         model = Business
         exclude = ['Admin', 'pub_date', 'admin_profile']
         widgets = {
-          'address': forms.Textarea(attrs={'rows':2, 'cols':10,}),
+          'address': forms.Textarea(attrs={'rows':1, 'cols':10,}),
         }
 
 class NewNeighborhoodForm(forms.ModelForm):
